c8856e94ed312d10162f31adeb0b573e28996d9c
alooshx99 <allawy2246@gmail.com>
2025-05-08 01:45:18 +0300

    reset password with gmail

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created! You can login now')
-        return redirect('login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form':form})
