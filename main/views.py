96ff8332f565e08a4b942e998ed5ecdd13097446
alooshx99 <allawy2246@gmail.com>
2025-05-07 21:54:35 +0300

    user posts page, with pagination, username links added

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.models import User
@@ -20,6 +20,17 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted'] # - for Reverse order
     paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
